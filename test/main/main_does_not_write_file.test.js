1587cfd0e198ddbf5623e3c7a773291db98ee357
Martin Joiner <martin@martinjoiner.co.uk>
2025-12-29 14:50:21 +0000

    Implementing Prettier on this source code
    Prettier was already a dependancy obviously but needed to add config for this codebase
    Also reformatted code to meet Prettier standards

--- test/main/main_does_not_write_file.test.js
+++ test/main/main_does_not_write_file.test.js
@@ -5,15 +5,15 @@ test('main() does not write file when write mode is false', async () => {
         findFiles: jest.fn().mockName('mockFindFiles').mockReturnValue(['file.bru']),
         readFile: jest.fn().mockName('mockReadFile').mockReturnValue('mock content'),
         writeFile: jest.fn().mockName('mockWriteFile'),
-    }));
+    }))
 
-    const {writeFile} = await import('../../lib/files.mjs');
+    const {writeFile} = await import('../../lib/files.mjs')
 
-    const {main} = await import('../../lib/main.mjs');
+    const {main} = await import('../../lib/main.mjs')
 
     const mockConsole = {log: jest.fn()}
 
     return main(mockConsole, '/home', 'bruno-collection', false).then(() => {
-        expect(writeFile).not.toHaveBeenCalled();
-    });
-});
+        expect(writeFile).not.toHaveBeenCalled()
+    })
+})
--- test/main/main_writes_file.test.js
+++ test/main/main_writes_file.test.js
@@ -2,20 +2,23 @@ import {jest, test, expect} from '@jest/globals'
 
 test('main() writes file when write mode is true', async () => {
     jest.unstable_mockModule('../../lib/files.mjs', () => ({
-        findFiles: jest.fn().mockName('mockFindFiles').mockReturnValue(['/home/bruno-collection/Simple GET Request.bru']),
+        findFiles: jest
+            .fn()
+            .mockName('mockFindFiles')
+            .mockReturnValue(['/home/bruno-collection/Simple GET Request.bru']),
         readFile: jest.fn().mockName('mockReadFile').mockReturnValue('mock file contents'),
         writeFile: jest.fn().mockName('mockWriteFile'),
-    }));
+    }))
 
-    const {readFile, writeFile} = await import('../../lib/files.mjs');
+    const {readFile, writeFile} = await import('../../lib/files.mjs')
 
     jest.unstable_mockModule('../../lib/format.mjs', () => ({
         formatBlocks: jest.fn().mockName('mockFormatBlocks').mockReturnValue({
             newContents: 'New file contents',
             changeable: true,
-            error_messages: []
+            error_messages: [],
         }),
-    }));
+    }))
 
     const {formatBlocks} = await import('../../lib/format.mjs')
 
@@ -24,8 +27,11 @@ test('main() writes file when write mode is true', async () => {
     const mockConsole = {log: jest.fn()}
 
     return main(mockConsole, '/home', 'bruno-collection', true).then(() => {
-        expect(readFile).toHaveBeenCalledTimes(1);
-        expect(formatBlocks).toHaveBeenCalledWith("mock file contents", null);
-        expect(writeFile).toHaveBeenCalledWith('/home/bruno-collection/Simple GET Request.bru', 'New file contents');
-    });
-});
+        expect(readFile).toHaveBeenCalledTimes(1)
+        expect(formatBlocks).toHaveBeenCalledWith('mock file contents', null)
+        expect(writeFile).toHaveBeenCalledWith(
+            '/home/bruno-collection/Simple GET Request.bru',
+            'New file contents'
+        )
+    })
+})
